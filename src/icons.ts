9fa6a329ec95f8c97b4b5641d0ee6447f6591364
Mizunashi Mana <mizunashi-mana@noreply.git>
2023-02-06 00:46:16 +0900

    Fix module name

--- src/icons.ts
+++ src/icons.ts
@@ -9,7 +9,7 @@ import { dom, library } from "@fortawesome/fontawesome-svg-core";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons/faEnvelope";
 import { faBook } from "@fortawesome/free-solid-svg-icons/faBook";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
-import { faGithub } from "@fortawesome/free-brands-svg-icons/faGitHub";
+import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons/faTwitter";
 import { faMastodon } from "@fortawesome/free-brands-svg-icons/faMastodon";
 
